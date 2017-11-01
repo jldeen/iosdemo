a5acb629fc10bba493309e50e58070975a023705
jldeen <jedeen@outlook.com>
2017-10-31 17:40:10 -0700

    Initial Commit

---
+++ Test/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Test
+//
+//  Created by Jessica L. Deen on 10/31/17.
+//  Copyright © 2017 Jessica L. Deen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Test/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Test
+//
+//  Created by Jessica L. Deen on 10/31/17.
+//  Copyright © 2017 Jessica L. Deen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
